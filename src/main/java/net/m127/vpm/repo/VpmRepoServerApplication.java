24bccb2b8c144cb3c1430cf070f3fb281df40485
Megacrafter127 <ee_player@web.de>
2022-10-27 23:18:46 +0200

    Disabled spring security as we handle security manually the 2nd.

--- src/main/java/net/m127/vpm/repo/VpmRepoServerApplication.java
+++ src/main/java/net/m127/vpm/repo/VpmRepoServerApplication.java
@@ -1,13 +1,20 @@
 package net.m127.vpm.repo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude =  {
+        SecurityAutoConfiguration.class,
+        ManagementWebSecurityAutoConfiguration.class
+    }
+)
 @EnableJpaRepositories
 public class VpmRepoServerApplication {
     
