02081b0b62d188da70fdacef929d597f453613ab
Megacrafter127 <ee_player@web.de>
2022-10-27 23:54:19 +0200

    Random Secret generation for tokens and validation fixes.

--- src/main/java/net/m127/vpm/repo/jwt/TokenManager.java
+++ src/main/java/net/m127/vpm/repo/jwt/TokenManager.java
@@ -3,27 +3,29 @@ package net.m127.vpm.repo.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class TokenManager {
-    private final String jwtSecret;
+    private final SecretKey jwtSecret;
     private final long tokenLifetime;
-    
     private final JwtParser parser;
     
     public TokenManager(
-        @Value("${jwt.tokenLifetime}") long lifetime,
-        @Value("${jwt.secret}") String base64
-    ) {
-        jwtSecret = base64;
+        @Value("${jwt.tokenLifetime}") long lifetime
+    ) throws NoSuchAlgorithmException {
         tokenLifetime = lifetime;
+        jwtSecret = KeyGenerator.getInstance("HmacSHA512")
+            .generateKey();
         parser = Jwts.parserBuilder().setSigningKey(jwtSecret).build();
     }
     
@@ -33,20 +35,28 @@ public class TokenManager {
             Jwts.builder().setClaims(claims).setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + tokenLifetime * 1000))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret).compact(),
+                .signWith(jwtSecret).compact(),
             tokenLifetime
         );
     }
     
     public boolean validateJwtToken(String token, String username) {
-        Claims claims = parser.parseClaimsJws(token).getBody();
-        boolean isTokenExpired = claims.getExpiration().before(new Date());
-        return (claims.getSubject().equals(username)) && !isTokenExpired;
+        try {
+            final Claims claims = parser.parseClaimsJws(token).getBody();
+            boolean isTokenExpired = claims.getExpiration().before(new Date());
+            return (claims.getSubject().equals(username)) && !isTokenExpired;
+        } catch(SignatureException ex) {
+            return false;
+        }
     }
     
     public String getUsernameFromValidToken(String token) {
-        final Claims claims = parser.parseClaimsJws(token).getBody();
-        if(claims.getExpiration().before(new Date())) return null;
-        return claims.getSubject();
+        try {
+            final Claims claims = parser.parseClaimsJws(token).getBody();
+            if(claims.getExpiration().before(new Date())) return null;
+            return claims.getSubject();
+        } catch(SignatureException ex) {
+            return null;
+        }
     }
 }
